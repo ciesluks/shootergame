853f933695adb0d21d1899ed90c7c690023ea6f5
simpelito <simonciesluk@gmail.com>
2018-02-11 00:20:34 +0100

    Started adding enemies

--- game.js
+++ game.js
@@ -16,6 +16,7 @@ var fireUpKey;
 var fireDownKey;
 var fireLeftKey;
 var fireRightKey;
+var enemies;
 
 window.onload = function() {
 		game = new Phaser.Game(600, 600, Phaser.CANVAS, "gameContainer",
@@ -39,8 +40,38 @@ function create(){
     fireRightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 
     player = new player(276,276);
+    setWeapon();
 
-    //  Creates 30 bullets, using the 'bullet' graphic
+    enemies = game.add.group();
+    for (var i = 0; i < 16; i++)
+    {
+        //  This creates a new Phaser.Sprite instance within the group
+        //  It will be randomly placed within the world and use the 'baddie' image to display
+        enemies.create(game.world.randomX, game.world.randomY, 'ufo_green');
+    }
+
+
+}
+
+function update(){
+    player.update();
+    fireWeapon();
+}
+
+function player(x,y) {
+    this.sprite = game.add.sprite(x, y, 'ufo_green')
+    this.speed = 2;
+    game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+	this.sprite.body.collideWorldBounds = true;
+    this.update = function() {
+        if (upKey.isDown) { player.sprite.y -= player.speed; }
+        else if (downKey.isDown) { player.sprite.y += player.speed; }
+        if (leftKey.isDown) { player.sprite.x -= player.speed; }
+        else if (rightKey.isDown) { player.sprite.x += player.speed; }
+    }
+}
+
+function setWeapon() {
     weapon = game.add.weapon(30, 'ufo_green');
     //  The bullet will be automatically killed when it leaves the world bounds
     weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
@@ -48,12 +79,10 @@ function create(){
     weapon.bulletSpeed = 400;
     //  Speed-up the rate of fire, allowing them to shoot 1 bullet every X ms
     weapon.fireRate = 200;
-
     weapon.trackSprite(player.sprite, 12, 12, false);
 }
 
-function update(){
-    player.move();
+function fireWeapon() {
     if (fireRightKey.isDown & fireDownKey.isDown)
     {
         weapon.fireAngle = 45;
@@ -96,15 +125,6 @@ function update(){
     }
 }
 
-function player(x,y) {
-    this.sprite = game.add.sprite(x, y, 'ufo_green')
-    this.speed = 2;
-    game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
-	this.sprite.body.collideWorldBounds = true;
-    this.move = function() {
-        if (upKey.isDown) { player.sprite.y -= player.speed; }
-        else if (downKey.isDown) { player.sprite.y += player.speed; }
-        if (leftKey.isDown) { player.sprite.x -= player.speed; }
-        else if (rightKey.isDown) { player.sprite.x += player.speed; }
-    }
+function enemy(x, y) {
+    this
 }
