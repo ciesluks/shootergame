bf48eeceb8cbdd214eefc110064a74d9da8f2402
simpelito <simonciesluk@gmail.com>
2018-02-16 23:42:46 +0100

    Improved enemy movement, killable

--- game.js
+++ game.js
@@ -18,6 +18,26 @@ var fireLeftKey;
 var fireRightKey;
 var enemies;
 
+function Enemy1(game,x,y) {
+    Phaser.Sprite.call(this, game, x, y, 'ufo_green');
+    game.physics.enable(this, Phaser.Physics.ARCADE);
+    this.body.bounce.setTo(1, 1);
+}
+Enemy1.prototype = Object.create(Phaser.Sprite.prototype);
+Enemy1.prototype.constructor = Enemy1;
+Enemy1.prototype.speed = 1;
+Enemy1.prototype.update = function() {
+    //  Automatically called by World.update
+    // Calculate direction towards player
+    toPlayerX = player.sprite.x - this.x;
+    toPlayerY = player.sprite.y - this.y;
+    toPlayerLength = Math.sqrt(toPlayerX * toPlayerX + toPlayerY * toPlayerY);
+    var angle = Math.atan2(toPlayerY, toPlayerX);
+
+    this.body.velocity.x = Math.cos(angle) * 50;
+    this.body.velocity.y = Math.sin(angle) * 50;
+}
+
 window.onload = function() {
 		game = new Phaser.Game(600, 600, Phaser.CANVAS, "gameContainer",
 		{ preload: preload, create: create, update: update });
@@ -43,21 +63,33 @@ function create(){
     player = new player(276,276);
     setWeapon();
 
-    enemies = [];
+    enemies = game.add.group();
+    enemies.classType = Enemy1;
 }
 
 function update(){
     player.update();
     fireWeapon();
-    if (enemies.length < 10)
-    {
-        var enemy = new enemy1(game.world.randomX, -24);
-        enemies.push(enemy);
+    if (enemies.length < 10) {
+        var enemy;
+        switch(Math.floor(Math.random()*4)) {
+            case 0:
+                enemy = new Enemy1(game, game.world.randomX, -100);
+                break;
+            case 1:
+                enemy = new Enemy1(game, game.world.randomX, 700);
+                break;
+            case 2:
+                enemy = new Enemy1(game, -100, game.world.randomY);
+                break;
+            case 3:
+                enemy = new Enemy1(game, 700, game.world.randomY);
+                break;
+        }
+        enemies.add(enemy);
     }
-    for (var i = 0; i < enemies.length; i++)
-    {
-        enemies[i].update();
-    }
+    game.physics.arcade.overlap(enemies, weapon.bullets, bulletCollisionHandler, null, this);
+    game.physics.arcade.collide(enemies);
 }
 
 function player(x,y) {
@@ -65,8 +97,7 @@ function player(x,y) {
     this.speed = 2;
     game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
 	this.sprite.body.collideWorldBounds = true;
-    this.update = function()
-    {
+    this.update = function() {
         if (upKey.isDown) { this.sprite.y -= this.speed; }
         else if (downKey.isDown) { this.sprite.y += this.speed; }
         if (leftKey.isDown) { this.sprite.x -= this.speed; }
@@ -74,31 +105,9 @@ function player(x,y) {
     }
 }
 
-function enemy1(x,y) {
-    this.sprite = game.add.sprite(x, y, 'ufo_green');
-    this.speed = 1;
-    game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
-    this.sprite.body.bounce.set(0.8);
-    this.sprite.body.immovable = true;
-    this.update = function()
-    {
-        if(this.sprite.x < player.sprite.x)
-        {
-            this.sprite.x += this.speed;
-        }
-        if(this.sprite.x > player.sprite.x)
-        {
-            this.sprite.x -= this.speed;
-        }
-        if(this.sprite.y < player.sprite.y)
-        {
-            this.sprite.y += this.speed;
-        }
-        if(this.sprite.y > player.sprite.y)
-        {
-            this.sprite.y -= this.speed;
-        }
-    }
+function bulletCollisionHandler(enemy, bullet) {
+    enemies.remove(enemy);
+    bullet.kill();
 }
 
 function setWeapon() {
@@ -108,48 +117,40 @@ function setWeapon() {
     //  The speed at which the bullet is fired
     weapon.bulletSpeed = 400;
     //  Speed-up the rate of fire, allowing them to shoot 1 bullet every X ms
-    weapon.fireRate = 200;
+    weapon.fireRate = 300;
     weapon.trackSprite(player.sprite, 12, 12, false);
 }
 
 function fireWeapon() {
-    if (fireRightKey.isDown & fireDownKey.isDown)
-    {
+    if (fireRightKey.isDown & fireDownKey.isDown) {
         weapon.fireAngle = 45;
         weapon.fire();
     }
-    else if (fireLeftKey.isDown & fireDownKey.isDown)
-    {
+    else if (fireLeftKey.isDown & fireDownKey.isDown) {
         weapon.fireAngle = 135;
         weapon.fire();
     }
-    else if (fireLeftKey.isDown & fireUpKey.isDown)
-    {
+    else if (fireLeftKey.isDown & fireUpKey.isDown) {
         weapon.fireAngle = 225;
         weapon.fire();
     }
-    else if (fireRightKey.isDown & fireUpKey.isDown)
-    {
+    else if (fireRightKey.isDown & fireUpKey.isDown) {
         weapon.fireAngle = 315;
         weapon.fire();
     }
-    else if (fireUpKey.isDown)
-    {
+    else if (fireUpKey.isDown) {
         weapon.fireAngle = Phaser.ANGLE_UP;
         weapon.fire();
     }
-    else if (fireDownKey.isDown)
-    {
+    else if (fireDownKey.isDown) {
         weapon.fireAngle = Phaser.ANGLE_DOWN;
         weapon.fire();
     }
-    else if (fireRightKey.isDown)
-    {
+    else if (fireRightKey.isDown) {
         weapon.fireAngle = Phaser.ANGLE_RIGHT;
         weapon.fire();
     }
-    else if (fireLeftKey.isDown)
-    {
+    else if (fireLeftKey.isDown) {
         weapon.fireAngle = Phaser.ANGLE_LEFT;
         weapon.fire();
     }
